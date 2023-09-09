1df7793485acf9e3a2129a5564d50362c577237f
fusionmovements <fusionmovements@gmail.com>
2023-09-09 11:29:15 +0200

    https

--- exercises-app/frontend/src/services/exercises.js
+++ exercises-app/frontend/src/services/exercises.js
@@ -2,19 +2,19 @@ import axios from "axios";
 
 class ExerciseDataService{
 getAll(page=0){
-    return axios.get(`http://backend-3s98.onrender.com/api/v1/exercises?page=${page}`)
+    return axios.get(`https://backend-3s98.onrender.com/api/v1/exercises?page=${page}`)
 }
 
 get(id){
-    return axios.get(`http://backend-3s98.onrender.com/api/v1/exercises/id/${id}`)
+    return axios.get(`https://backend-3s98.onrender.com/api/v1/exercises/id/${id}`)
 }
 
 find(query,by){
-    return axios.get(`http://backend-3s98.onrender.com/api/v1/exercises?${by}=${query}`)
+    return axios.get(`https://backend-3s98.onrender.com/api/v1/exercises?${by}=${query}`)
 }
 
 find2(query1,by1,query2,by2){
-    return axios.get(`http://backend-3s98.onrender.com/api/v1/exercises?${by1}=${query1}&${by2}=${query2}`)
+    return axios.get(`https://backend-3s98.onrender.com/api/v1/exercises?${by1}=${query1}&${by2}=${query2}`)
 }
 
 //findTwo(query1,by1,query2,by2){}
@@ -32,11 +32,11 @@ find2(query1,by1,query2,by2){
 // }
 
 getGroups(){
-    return axios.get(`http://backend-3s98.onrender.com/api/v1/exercises/groups`)
+    return axios.get(`https://backend-3s98.onrender.com/api/v1/exercises/groups`)
 }
 
 getEquipments(){
-    return axios.get(`http://backend-3s98.onrender.comapi/v1/exercises/equipments`)
+    return axios.get(`https://backend-3s98.onrender.comapi/v1/exercises/equipments`)
 }
 
 
